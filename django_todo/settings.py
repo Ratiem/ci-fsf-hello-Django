2fc21cb92f604abacbbd99f94103d4e1ffca747d
Ratiem <ratidzo.makoni@gmail.com>
2023-05-19 16:01:13 +0000

    Adjusted env.py file

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ratiem-django-todo-app.herokuapp.com']
+ALLOWED_HOSTS = ['ratiem-django-todo-app.herokuapp.com', '8000-ratiem-cifsfhellodjango-nm8pnx8xpl7.ws-eu97.gitpod.io']
 
 
 # Application definition
